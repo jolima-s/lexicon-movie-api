c5af4d009a1e14209e7ed82557795d32866471ea
Johanna Sewring <j_sewring@hotmail.com>
2026-06-24 14:40:02 +0200

    actors controller and reviews controller

---
+++ LexiconMovieApi/Controllers/ActorsController.cs
@@ -0,0 +1,121 @@
+﻿using LexiconMovieApi.Core.DTOs.Actor;
+using LexiconMovieApi.Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconMovieApi.Client.Controllers;
+
+[Route("api/actors")]
+[ApiController]
+public class ActorsController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public ActorsController(IServiceManager serviceManager) => _serviceManager = serviceManager;
+
+    // GET: api/actors
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ActorDto>>> GetActors()
+    {
+        var actors = await _serviceManager.ActorService.GetAllActorsAsync();
+
+        return Ok(actors);
+    }
+
+    // GET: api/actors/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ActorDto>> GetActor(int id)
+    {
+        var actor = await _serviceManager.ActorService.GetActorByIdAsync(id);
+
+        if (actor == null)
+            return NotFound();
+
+        return Ok(actor);
+    }
+
+    // PUT: api/actors/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutActor(int? id, ActorUpdateDto actor)
+    {
+        if (id != actor.Id)
+            return BadRequest();
+
+        try
+        {
+            await _serviceManager.ActorService.UpdateActorAsync(actor);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while updating the actor.");
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/actors
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<ActorDto>> PostActor(ActorCreateDto actor)
+    {
+        ActorDto entity;
+
+        try
+        {
+            entity = await _serviceManager.ActorService.CreateActorAsync(actor);
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while creating the actor.");
+        }
+
+        return CreatedAtAction("GetActor", new { id = entity.Id }, entity);
+    }
+
+    // DELETE: api/actors/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActor(int? id)
+    {
+        if (id == null)
+            return BadRequest();
+
+        try
+        {
+            await _serviceManager.ActorService.DeleteActorAsync(id.Value);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while deleting the actor.");
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/movies/2/actors/4
+    [HttpPost("api/movies/{movieId}/actors/{actorId}")]
+    public async Task<IActionResult> AddActorToMovie(int movieId, int actorId)
+    {
+        try
+        {
+            await _serviceManager.ActorService.AddActorToMovieAsync(movieId, actorId);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while adding the actor to the movie.");
+        }
+
+        return NoContent();
+    }
+}
---
+++ LexiconMovieApi/Controllers/ReviewsController.cs
@@ -0,0 +1,103 @@
+﻿using LexiconMovieApi.Core.DTOs.Movie;
+using LexiconMovieApi.Core.DTOs.Review;
+using LexiconMovieApi.Data.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconMovieApi.Client.Controllers;
+
+[Route("api/reviews")]
+[ApiController]
+public class ReviewsController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public ReviewsController(IServiceManager serviceManager) => _serviceManager = serviceManager;
+
+    // GET: api/reviews
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ReviewDto>>> GetReviews()
+    {
+        var reviews = await _serviceManager.ReviewService.GetAllReviewsAsync();
+
+        return Ok(reviews);
+    }
+
+    // GET: api/reviews/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ReviewDto>> GetReview(int id)
+    {
+        var review = await _serviceManager.ReviewService.GetReviewByIdAsync(id);
+
+        if (review == null)
+            return NotFound();
+
+        return Ok(review);
+    }
+
+    // PUT: api/reviews/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutReview(int? id, ReviewUpdateDto review)
+    {
+        if (id != review.Id)
+            return BadRequest();
+
+        try
+        {
+            await _serviceManager.ReviewService.UpdateReviewAsync(review);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while updating the review.");
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/reviews
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<ReviewDto>> PostReview(ReviewCreateDto review)
+    {
+        ReviewDto entity;
+
+        try
+        {
+            entity = await _serviceManager.ReviewService.CreateReviewAsync(review);
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while creating the review.");
+        }
+
+        return CreatedAtAction("GetReview", entity);
+    }
+
+    // DELETE: api/reviews/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(int? id)
+    {
+        if (id == null)
+            return BadRequest();
+
+        try
+        {
+            await _serviceManager.ReviewService.DeleteReviewAsync(id.Value);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while deleting the review.");
+        }
+
+        return NoContent();
+    }
+}
